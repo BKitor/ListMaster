bf227576cef7f898d68ad9ad20b4720f4c172815
Kitorus <bkitor@gmail.com>
2019-01-27 18:43:26 -0500

    undo impliemnted on !clearlist

--- commands/clearlist.js
+++ commands/clearlist.js
@@ -2,6 +2,7 @@ const list = require("../list.json");
 const listFuncs = require("../listFuncs.js");
 
 exports.run=(client, message, args)=>{
+  list.latestAddition = list.list;
   list.list = [];
   listFuncs.saveList();
   listFuncs.reprintList(message);
