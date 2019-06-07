b9f720708485394ec9fa3a07ff5662682fbe6a81
Kitorus <bkitor@gmail.com>
2019-06-07 12:57:34 -0400

    autoformat stuff

--- listFuncs.js
+++ listFuncs.js
@@ -2,35 +2,35 @@
 const fs = require("fs");
 const list = require("./list.json");
 
-const clearChat = async (msg)=>{
-    let msgs;
-    do{
-      msgs = await msg.channel.fetchMessages({limit:50});
-      msg.channel.bulkDelete(msgs).catch(console.error);
-    }while(msgs.size>=2);
-    console.log("cleared Chat");
+const clearChat = async (msg) => {
+  let msgs;
+  do {
+    msgs = await msg.channel.fetchMessages({ limit: 50 });
+    msg.channel.bulkDelete(msgs).catch(console.error);
+  } while (msgs.size >= 2);
+  console.log("cleared Chat");
 }
-exports.clearChat=clearChat;
+exports.clearChat = clearChat;
 
-const printList=(msg)=>{
-    list.list.forEach((listItem, index)=>{
-      msg.channel.send(index+1+". "+listItem).catch(err=>console.error);
-    });
-    msg.channel.send("done");
+const printList = (msg) => {
+  list.list.forEach((listItem, index) => {
+    msg.channel.send(index + 1 + ". " + listItem).catch(err => console.error);
+  });
+  msg.channel.send("done");
 }
-exports.printList=printList;
+exports.printList = printList;
 
-exports.reprintList=async (msg)=>{
+exports.reprintList = async (msg) => {
   await clearChat(msg);
   printList(msg);
 }
 
-exports.saveList=async()=>{
+exports.saveList = async () => {
   fs.writeFile('list.json', JSON.stringify(list), finished);
 }
 
 
-function finished(err){
-  if(err)console.error(err);
+function finished(err) {
+  if (err) console.error(err);
   console.log('File saved');
 }
